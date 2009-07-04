efabd1b823bdf90c6ac49e3ba34d2ba5c8506b21
Zariel <c.bannister@gmail.com>
2009-07-04 19:37:37 +0100

    Move license into its own file

--- core.lua
+++ core.lua
@@ -1,30 +1,3 @@
---[[
-Copyright (c) 2008 Chris Bannister,
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-1. Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-3. The name of the author may not be used to endorse or promote products
-   derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-]]
-
 local _G = getfenv(0)
 local oUF = _G.oufgrid or _G.oUF
 
@@ -279,6 +252,7 @@ for i = 1, 8 do
 	local r = oUF:Spawn("header", "oUF_Raid" .. i)
 	r:SetPoint("TOP", UIParent, "TOP", 0, -500)
 	if i == 1 then
+		-- Change this to move it
 		r:SetPoint("LEFT", UIParent, "LEFT", 10, 0)
 		r:SetAttribute("showParty", true)
 		r:SetAttribute("showPlayer", true)
