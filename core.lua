26599ed66f0983e93b5fb819037a706cf7d5c01b
Chris Bannister <c.bannister@gmail.com>
2010-12-09 14:16:48 +0000

    Reformat

--- core.lua
+++ core.lua
@@ -2,7 +2,7 @@ local _G = getfenv(0)
 local oUF = _G.oufgrid or _G.oUF
 
 if not oUF then
-	return error("oUF_Grid requires oUF")
+    return error("oUF_Grid requires oUF")
 end
 
 local grid = setmetatable({}, { __index = oUF })
@@ -22,241 +22,241 @@ local texture = [[Interface\AddOns\oUF_Grid\media\gradient32x32.tga]]
 local hightlight = [[Interface\AddOns\oUF_Grid\media\mouseoverHighlight.tga]]
 
 local colors = {
-	class ={
-		-- I accept patches you know
-		["DEATHKNIGHT"] = { 0.77, 0.12, 0.23 },
-		["DRUID"] = { 1.0 , 0.49, 0.04 },
-		["HUNTER"] = { 0.67, 0.83, 0.45 },
-		["MAGE"] = { 0.41, 0.8 , 0.94 },
-		["PALADIN"] = { 0.96, 0.55, 0.73 },
-		["PRIEST"] = { 1.0 , 1.0 , 1.0 },
-		["ROGUE"] = { 1.0 , 0.96, 0.41 },
-		["SHAMAN"] = { 0,0.86,0.73 },
-		["WARLOCK"] = { 0.58, 0.51, 0.7 },
-		["WARRIOR"] = { 0.78, 0.61, 0.43 },
-	},
+    class ={
+        -- I accept patches you know
+        ["DEATHKNIGHT"] = { 0.77, 0.12, 0.23 },
+        ["DRUID"] = { 1.0 , 0.49, 0.04 },
+        ["HUNTER"] = { 0.67, 0.83, 0.45 },
+        ["MAGE"] = { 0.41, 0.8 , 0.94 },
+        ["PALADIN"] = { 0.96, 0.55, 0.73 },
+        ["PRIEST"] = { 1.0 , 1.0 , 1.0 },
+        ["ROGUE"] = { 1.0 , 0.96, 0.41 },
+        ["SHAMAN"] = { 0,0.86,0.73 },
+        ["WARLOCK"] = { 0.58, 0.51, 0.7 },
+        ["WARRIOR"] = { 0.78, 0.61, 0.43 },
+    },
 }
 setmetatable(colors.class, {
-	__index = function(self, key)
-		return { 0.78, 0.61, 0.43 }
-	end
+    __index = function(self, key)
+        return { 0.78, 0.61, 0.43 }
+    end
 })
 
 local GetClassColor = function(unit)
-	return unpack(colors.class[select(2, UnitClass(unit))])
+    return unpack(colors.class[select(2, UnitClass(unit))])
 end
 
 local ColorGradient = function(perc, r1, g1, b1, r2, g2, b2, r3, g3, b3)
-	if perc >= 1 then
-		return { r3, g3, b3 }
-	elseif perc <= 0 then
-		return { r1, g1, b1 }
-	end
+    if perc >= 1 then
+        return { r3, g3, b3 }
+    elseif perc <= 0 then
+        return { r1, g1, b1 }
+    end
 
-	local segment, relperc = math.modf(perc*(2))
-	local offset = (segment*3)+1
+    local segment, relperc = math.modf(perc*(2))
+    local offset = (segment*3)+1
 
-	if(offset == 1) then
-		return { r1 + (r2-r1)*relperc, g1 + (g2-g1)*relperc, b1 + (b2-b1)*relperc }
-	end
+    if(offset == 1) then
+        return { r1 + (r2-r1)*relperc, g1 + (g2-g1)*relperc, b1 + (b2-b1)*relperc }
+    end
 
-	return { r2 + (r3-r2)*relperc, g2 + (g3-g2)*relperc, b2 + (b3-b2)*relperc }
+    return { r2 + (r3-r2)*relperc, g2 + (g3-g2)*relperc, b2 + (b3-b2)*relperc }
 end
 
 local guidCache = setmetatable({}, {
-	__index = function(self, name)
-		self[name] = UnitGUID(name)
-	end
+    __index = function(self, name)
+        self[name] = UnitGUID(name)
+    end
 })
 
 local Name_Update = function(self, event, unit)
-	if self.unit ~= unit then return end
+    if self.unit ~= unit then return end
 
-	--self:Reset()
+    --self:Reset()
 
-	local n, s = UnitName(unit)
-	self.guid = guidCache[unit]
-	self.name = string.utf8sub(n, 1, 3)
+    local n, s = UnitName(unit)
+    self.guid = guidCache[unit]
+    self.name = string.utf8sub(n, 1, 3)
 
-	if UnitIsDeadOrGhost(unit) or not UnitIsConnected(unit) then
-		self.Health.bg:SetVertexColor(0.3, 0.3, 0.3)
-	else
-		self.Health.bg:SetVertexColor(GetClassColor(unit))
-	end
+    if UnitIsDeadOrGhost(unit) or not UnitIsConnected(unit) then
+        self.Health.bg:SetVertexColor(0.3, 0.3, 0.3)
+    else
+        self.Health.bg:SetVertexColor(GetClassColor(unit))
+    end
 end
 
 local round = function(x, y)
-	return math.floor((x * 10 ^ y)+ 0.5) / 10 ^ y
+    return math.floor((x * 10 ^ y)+ 0.5) / 10 ^ y
 end
 
 local Health_Update = function(self, event, unit, bar, current, max)
-	local def = max - current
-	bar:SetValue(current)
+    local def = max - current
+    bar:SetValue(current)
 
-	local per = round(current/max, 100)
-	local col = ColorGradient(per, 1, 0, 0, 1, 1, 0, 1, 1, 1)
-	self.Name:SetTextColor(unpack(col))
+    local per = round(current/max, 100)
+    local col = ColorGradient(per, 1, 0, 0, 1, 1, 0, 1, 1, 1)
+    self.Name:SetTextColor(unpack(col))
 
-	if per > 0.9 or UnitIsDeadOrGhost(unit) then
-		self.Name:SetText(self.name)
-	else
-		self.Name:SetFormattedText("-%0.1f", math.floor(def/100)/10)
-	end
+    if per > 0.9 or UnitIsDeadOrGhost(unit) then
+        self.Name:SetText(self.name)
+    else
+        self.Name:SetFormattedText("-%0.1f", math.floor(def/100)/10)
+    end
 
-	if UnitIsDeadOrGhost(unit) or not UnitIsConnected(unit) then
-		bar.bg:SetVertexColor(0.3, 0.3, 0.3)
-	else
-		bar.bg:SetVertexColor(GetClassColor(unit))
-	end
+    if UnitIsDeadOrGhost(unit) or not UnitIsConnected(unit) then
+        bar.bg:SetVertexColor(0.3, 0.3, 0.3)
+    else
+        bar.bg:SetVertexColor(GetClassColor(unit))
+    end
 
-	-- Hopefully this fixes everything ...
-	if self.UpdateHeals then
-		self:UpdateHeals(self.guid)
-	end
+    -- Hopefully this fixes everything ...
+    if self.UpdateHeals then
+        self:UpdateHeals(self.guid)
+    end
 end
 
 local reset = function(self)
-	self.border:Hide()
-	self.Icon:Hide()
-	self.Dispell = false
+    self.border:Hide()
+    self.Icon:Hide()
+    self.Dispell = false
 end
 
 local OnEnter = function(self)
-	UnitFrame_OnEnter(self)
-	self.Highlight:Show()
+    UnitFrame_OnEnter(self)
+    self.Highlight:Show()
 end
 
 local OnLeave = function(self)
-	UnitFrame_OnLeave(self)
-	self.Highlight:Hide()
+    UnitFrame_OnLeave(self)
+    self.Highlight:Hide()
 end
 
 local frame = function(settings, self, unit)
-	self.menu = menu
+    self.menu = menu
 
-	self:EnableMouse(true)
+    self:EnableMouse(true)
 
-	self:SetScript("OnEnter", OnEnter)
-	self:SetScript("OnLeave", OnLeave)
+    self:SetScript("OnEnter", OnEnter)
+    self:SetScript("OnLeave", OnLeave)
 
-	self:RegisterForClicks("anyup")
+    self:RegisterForClicks("anyup")
 
-	self:SetBackdrop({
-		bgFile = "Interface\\Tooltips\\UI-Tooltip-Background", tile = true, tileSize = 16,
-		insets = { left = -2, right = -2, top = -2, bottom = -2 },
-	})
+    self:SetBackdrop({
+        bgFile = "Interface\\Tooltips\\UI-Tooltip-Background", tile = true, tileSize = 16,
+        insets = { left = -2, right = -2, top = -2, bottom = -2 },
+    })
 
-	self:SetBackdropColor(0, 0, 0, 1)
+    self:SetBackdropColor(0, 0, 0, 1)
 
-	self:SetAttribute("*type2", "menu")
+    self:SetAttribute("*type2", "menu")
 
-	local hp = CreateFrame("StatusBar", nil, self)
-	hp:SetAllPoints(self)
-	hp:SetStatusBarTexture(texture)
-	hp:SetOrientation("VERTICAL")
-	-- hp:SetFrameLevel(5)
-	hp:SetStatusBarColor(0, 0, 0, 0.75)
-	--hp:SetAlpha(0.)
+    local hp = CreateFrame("StatusBar", nil, self)
+    hp:SetAllPoints(self)
+    hp:SetStatusBarTexture(texture)
+    hp:SetOrientation("VERTICAL")
+    -- hp:SetFrameLevel(5)
+    hp:SetStatusBarColor(0, 0, 0, 0.75)
+    --hp:SetAlpha(0.)
 
-	local hpbg = hp:CreateTexture(nil, "BACKGROUND")
-	hpbg:SetAllPoints(hp)
-	hpbg:SetTexture(texture)
-	hpbg:SetAlpha(1)
+    local hpbg = hp:CreateTexture(nil, "BACKGROUND")
+    hpbg:SetAllPoints(hp)
+    hpbg:SetTexture(texture)
+    hpbg:SetAlpha(1)
 
-	local heal = hp:CreateTexture(nil, "BORDER")
-	heal:SetWidth(size)
-	heal:SetPoint("BOTTOM", self, "BOTTOM")
-	heal:SetPoint("LEFT", self, "LEFT")
-	heal:SetPoint("RIGHT", self, "RIGHT")
-	heal:SetTexture(texture)
-	heal:SetVertexColor(0, 1, 0, 0.8)
-	heal:Hide()
+    local heal = hp:CreateTexture(nil, "BORDER")
+    heal:SetWidth(size)
+    heal:SetPoint("BOTTOM", self, "BOTTOM")
+    heal:SetPoint("LEFT", self, "LEFT")
+    heal:SetPoint("RIGHT", self, "RIGHT")
+    heal:SetTexture(texture)
+    heal:SetVertexColor(0, 1, 0, 0.8)
+    heal:Hide()
 
-	self.heal = heal
+    self.heal = heal
 
-	hp.bg = hpbg
-	self.Health = hp
-	self.OverrideUpdateHealth = Health_Update
+    hp.bg = hpbg
+    self.Health = hp
+    self.OverrideUpdateHealth = Health_Update
 
-	local hl = hp:CreateTexture(nil, "OVERLAY")
-	hl:SetAllPoints(self)
-	hl:SetTexture("Interface\\QuestFrame\\UI-QuestTitleHighlight")
-	hl:SetBlendMode("ADD")
-	hl:Hide()
+    local hl = hp:CreateTexture(nil, "OVERLAY")
+    hl:SetAllPoints(self)
+    hl:SetTexture("Interface\\QuestFrame\\UI-QuestTitleHighlight")
+    hl:SetBlendMode("ADD")
+    hl:Hide()
 
-	self.Highlight = hl
+    self.Highlight = hl
 
-	local name = hp:CreateFontString(nil, "ARTWORK")
-	name:SetAlpha(1)
-	name:SetPoint("CENTER")
-	name:SetJustifyH("CENTER")
-	name:SetFont(supernova, 10, "THINOUTLINE")
-	name:SetShadowColor(0,0,0,1)
-	name:SetShadowOffset(1, -1)
-	name:SetTextColor(1, 1, 1, 1)
+    local name = hp:CreateFontString(nil, "ARTWORK")
+    name:SetAlpha(1)
+    name:SetPoint("CENTER")
+    name:SetJustifyH("CENTER")
+    name:SetFont(supernova, 10, "THINOUTLINE")
+    name:SetShadowColor(0,0,0,1)
+    name:SetShadowOffset(1, -1)
+    name:SetTextColor(1, 1, 1, 1)
 
-	self.Name = name
-	self.UNIT_NAME_UPDATE = Name_Update
+    self.Name = name
+    self.UNIT_NAME_UPDATE = Name_Update
 
-	local border = self:CreateTexture(nil, "ARTWORK")
-	border:SetPoint("LEFT", self, "LEFT", - 5, 0)
-	border:SetPoint("RIGHT", self, "RIGHT", 5, 0)
-	border:SetPoint("TOP", self, "TOP", 0, 5)
-	border:SetPoint("BOTTOM", self, "BOTTOM", 0, - 5)
-	border:SetTexture([[Interface\AddOns\oUF_Grid\media\Normal.tga]])
-	border:SetAlpha(1)
-	border:SetVertexColor(1, 1, 1)
-	border:Hide()
+    local border = self:CreateTexture(nil, "ARTWORK")
+    border:SetPoint("LEFT", self, "LEFT", - 5, 0)
+    border:SetPoint("RIGHT", self, "RIGHT", 5, 0)
+    border:SetPoint("TOP", self, "TOP", 0, 5)
+    border:SetPoint("BOTTOM", self, "BOTTOM", 0, - 5)
+    border:SetTexture([[Interface\AddOns\oUF_Grid\media\Normal.tga]])
+    border:SetAlpha(1)
+    border:SetVertexColor(1, 1, 1)
+    border:Hide()
 
-	self.border = border
+    self.border = border
 
-	local icon = hp:CreateTexture(nil, "OVERLAY")
-	icon:SetPoint("CENTER")
-	icon:SetAlpha(1)
-	icon:SetHeight(16)
-	icon:SetWidth(16)
-	icon:SetTexCoord(0.07, 0.93, 0.07, 0.93)
-	icon:Hide()
+    local icon = hp:CreateTexture(nil, "OVERLAY")
+    icon:SetPoint("CENTER")
+    icon:SetAlpha(1)
+    icon:SetHeight(16)
+    icon:SetWidth(16)
+    icon:SetTexCoord(0.07, 0.93, 0.07, 0.93)
+    icon:Hide()
 
-	local show, hide = icon.Show, icon.Hide
-	icon.Show = function(self)
-		show(self)
-		name:Hide()
-	end
+    local show, hide = icon.Show, icon.Hide
+    icon.Show = function(self)
+        show(self)
+        name:Hide()
+    end
 
-	icon.Hide = function(self)
-		hide(self)
-		name:Show()
-	end
+    icon.Hide = function(self)
+        hide(self)
+        name:Show()
+    end
 
-	self.Icon = icon
+    self.Icon = icon
 
-	local cd = CreateFrame("Cooldown", nil, self)
-	cd:SetAllPoints(icon)
-	cd:SetFrameLevel(7)
-	cd.noCooldownCount = true
+    local cd = CreateFrame("Cooldown", nil, self)
+    cd:SetAllPoints(icon)
+    cd:SetFrameLevel(7)
+    cd.noCooldownCount = true
 
-	self.cd = cd
+    self.cd = cd
 
-	self.Range = true
-	self.inRangeAlpha = 1
-	self.outsideRangeAlpha = 0.4
+    self.Range = true
+    self.inRangeAlpha = 1
+    self.outsideRangeAlpha = 0.4
 
-	self.incHeal = 0
-	self.healMod = 0
+    self.incHeal = 0
+    self.healMod = 0
 
-	self.frequentUpdates = true
+    self.frequentUpdates = true
 
-	self.Reset = reset
+    self.Reset = reset
 
-	return self
+    return self
 end
 
 local style = setmetatable({
-	["initial-height"] = size,
-	["initial-width"] = size,
+    ["initial-height"] = size,
+    ["initial-width"] = size,
 }, {
-	__call = frame,
+    __call = frame,
 })
 
 grid:RegisterStyle("Kanne-Grid", style)
--- events.lua
+++ events.lua
@@ -3,7 +3,7 @@ local _G = getfenv(0)
 local oUF = _G.oufgrid or _G.oUF
 
 if not oUF then
-	return error("oUF_Grid requires oUF")
+    return error("oUF_Grid requires oUF")
 end
 
 local libheal = LibStub("LibHealComm-4.0", true)
@@ -18,7 +18,7 @@ local GetTime = GetTime
 
 local f = CreateFrame("Frame")
 f:SetScript("OnEvent", function(self, event, ...)
-	return self[event](self, event, ...)
+    return self[event](self, event, ...)
 end)
 
 local playerClass = select(2, UnitClass("player"))
@@ -31,286 +31,286 @@ local UpdateRoster
 local size = oUF.size
 
 if playerName == "Kanne" then
-	playerClass = "PALADIN"
+    playerClass = "PALADIN"
 end
 
 -- spell = priority
 local debuffs = setmetatable({
-	["Viper Sting"] = 7,
+    ["Viper Sting"] = 7,
 
-	["Wound Poison"] = 9,
-	["Mortal Strike"] = 8,
-	["Aimed Shot"] = 8,
+    ["Wound Poison"] = 9,
+    ["Mortal Strike"] = 8,
+    ["Aimed Shot"] = 8,
 
-	["Counterspell - Silenced"] = 11,
-	["Counterspell"] = 10,
+    ["Counterspell - Silenced"] = 11,
+    ["Counterspell"] = 10,
 
-	["Blind"] = 10,
-	["Cyclone"] = 10,
-	["Scatter Shot"] = 10,
+    ["Blind"] = 10,
+    ["Cyclone"] = 10,
+    ["Scatter Shot"] = 10,
 
-	["Polymorph"] = 7,
+    ["Polymorph"] = 7,
 
-	["Entangling Roots"] = 7,
-	["Freezing Trap Effect"] = 7,
-	["Chains of Ice"] = 7,
+    ["Entangling Roots"] = 7,
+    ["Freezing Trap Effect"] = 7,
+    ["Chains of Ice"] = 7,
 
-	["Crippling Poison"] = 6,
-	["Hamstring"] = 5,
-	["Wingclip"] = 5,
+    ["Crippling Poison"] = 6,
+    ["Hamstring"] = 5,
+    ["Wingclip"] = 5,
 
-	["Fear"] = 3,
-	["Psycic Scream"] = 3,
-	["Howl of Terror"] = 3,
+    ["Fear"] = 3,
+    ["Psycic Scream"] = 3,
+    ["Howl of Terror"] = 3,
 }, { __index = function() return 0 end })
 
 local dispellPriority = {
-	["Magic"] = 4,
-	["Poison"] = 3,
-	["Disease"] = 1,
-	["Curse"] = 2,
-	["None"] = 0,
+    ["Magic"] = 4,
+    ["Poison"] = 3,
+    ["Disease"] = 1,
+    ["Curse"] = 2,
+    ["None"] = 0,
 }
 
 local dispellClass
 do
-	local t = {
-		["PRIEST"] = {
-			["Magic"] = true,
-			["Disease"] = true,
-		},
-		["SHAMAN"] = {
-			["Poison"] = true,
-			["Disease"] = true,
-			["Curse"] = true,
-		},
-		["PALADIN"] = {
-			["Poison"] = true,
-			["Magic"] = true,
-			["Disease"] = true,
-		},
-		["MAGE"] = {
-			["Curse"] = true,
-		},
-		["DRUID"] = {
-			["Curse"] = true,
-			["Poison"] = true,
-		},
-	}
-	if t[playerClass] then
-		dispellClass = {}
-		for k, v in pairs(t[playerClass]) do
-			dispellClass[k] = v
-			dispellPriority[k] = dispellPriority[k] * 10
-		end
-		t = nil
-	end
+    local t = {
+        ["PRIEST"] = {
+            ["Magic"] = true,
+            ["Disease"] = true,
+        },
+        ["SHAMAN"] = {
+            ["Poison"] = true,
+            ["Disease"] = true,
+            ["Curse"] = true,
+        },
+        ["PALADIN"] = {
+            ["Poison"] = true,
+            ["Magic"] = true,
+            ["Disease"] = true,
+        },
+        ["MAGE"] = {
+            ["Curse"] = true,
+        },
+        ["DRUID"] = {
+            ["Curse"] = true,
+            ["Poison"] = true,
+        },
+    }
+    if t[playerClass] then
+        dispellClass = {}
+        for k, v in pairs(t[playerClass]) do
+            dispellClass[k] = v
+            dispellPriority[k] = dispellPriority[k] * 10
+        end
+        t = nil
+    end
 end
 
 -- Lib Heal Support
 
 if libheal then
-	local heals = {}
-	local roster = libheal:GetGUIDUnitMapTable()
+    local heals = {}
+    local roster = libheal:GetGUIDUnitMapTable()
 
-	function heals:HealComm_HealStopped(event, casterGUID, spellID, type, interuptted, ...)
-		self:HealComm_HealStarted(nil, nil, nil, nil, nil, ...)
-	end
+    function heals:HealComm_HealStopped(event, casterGUID, spellID, type, interuptted, ...)
+        self:HealComm_HealStarted(nil, nil, nil, nil, nil, ...)
+    end
 
-	function heals:HealComm_ModifierChanged(event, guid)
-		self:UpdateHeals(nil, nil, nil, nil, nil, guid)
-	end
+    function heals:HealComm_ModifierChanged(event, guid)
+        self:UpdateHeals(nil, nil, nil, nil, nil, guid)
+    end
 
-	function heals:HealComm_HealDelayed(event, healerName, healSize, endTime, ...)
-		self:HealComm_HealStarted(event, healerName, healSize, endTime, ...)
-	end
+    function heals:HealComm_HealDelayed(event, healerName, healSize, endTime, ...)
+        self:HealComm_HealStarted(event, healerName, healSize, endTime, ...)
+    end
 
-	heals.HealComm_HealUpdated = heals.HealComm_HealDelayed
+    heals.HealComm_HealUpdated = heals.HealComm_HealDelayed
 
-	function heals:HealComm_HealStarted(event, healerGUID, spellID, healType, endTime, ...)
-		for i = 1, select("#", ...) do
-			self:UpdateHeals(select(i, ...))
-		end
-	end
+    function heals:HealComm_HealStarted(event, healerGUID, spellID, healType, endTime, ...)
+        for i = 1, select("#", ...) do
+            self:UpdateHeals(select(i, ...))
+        end
+    end
 
-	local mod, incPer, per, incSize, size, max
-	function heals:GetIncSize(guid, unit)
-		local incHeal = libheal:GetHealAmount(guid, libheal.ALL_HEALS)
+    local mod, incPer, per, incSize, size, max
+    function heals:GetIncSize(guid, unit)
+        local incHeal = libheal:GetHealAmount(guid, libheal.ALL_HEALS)
 
-		if incHeal then
-			max = UnitHealthMax(unit)
-			mod = libheal:GetHealModifier(guid)
-			incPer = (mod * incHeal) / max
-			per = UnitHealth(unit) / max
-			incSize = incPer * oUF.size
-			size = oUF.size * per
+        if incHeal then
+            max = UnitHealthMax(unit)
+            mod = libheal:GetHealModifier(guid)
+            incPer = (mod * incHeal) / max
+            per = UnitHealth(unit) / max
+            incSize = incPer * oUF.size
+            size = oUF.size * per
 
-			if incSize + size >= oUF.size then
-				incSize = oUF.size - size
-			end
+            if incSize + size >= oUF.size then
+                incSize = oUF.size - size
+            end
 
-			return incSize, incHeal, mod
-		else
-			return
-		end
-	end
+            return incSize, incHeal, mod
+        else
+            return
+        end
+    end
 
-	function heals:UpdateHeals(guid)
-		local unit = roster[guid]
-		if not oUF.units[unit] then return end
+    function heals:UpdateHeals(guid)
+        local unit = roster[guid]
+        if not oUF.units[unit] then return end
 
-		local frame = oUF.units[unit]
+        local frame = oUF.units[unit]
 
-		local max, current = UnitHealth(unit), UnitHealthMax(unit)
-		if current == max then
-			return frame.heal:Hide()
-		end
+        local max, current = UnitHealth(unit), UnitHealthMax(unit)
+        if current == max then
+            return frame.heal:Hide()
+        end
 
-		local incSize, incHeal, healMod = heals:GetIncSize(guid, unit)
-		if incSize then
-			local size = oUF.size * (UnitHealth(unit) / UnitHealthMax(unit))
-			frame.heal:SetHeight(incSize)
-			frame.heal:SetPoint("BOTTOM", frame, "BOTTOM", 0, size)
-			frame.heal:SetPoint("LEFT", frame, "LEFT")
-			frame.heal:SetPoint("RIGHT", frame, "RIGHT")
-			frame.heal:Show()
-		else
-			frame.heal:Hide()
-		end
+        local incSize, incHeal, healMod = heals:GetIncSize(guid, unit)
+        if incSize then
+            local size = oUF.size * (UnitHealth(unit) / UnitHealthMax(unit))
+            frame.heal:SetHeight(incSize)
+            frame.heal:SetPoint("BOTTOM", frame, "BOTTOM", 0, size)
+            frame.heal:SetPoint("LEFT", frame, "LEFT")
+            frame.heal:SetPoint("RIGHT", frame, "RIGHT")
+            frame.heal:Show()
+        else
+            frame.heal:Hide()
+        end
 
-		frame.incHeal = incHeal or 0
-		frame.healMod = healMod or 1
-	end
+        frame.incHeal = incHeal or 0
+        frame.healMod = healMod or 1
+    end
 
-	oUF.UpdateHeals = heals.UpdateHeals
+    oUF.UpdateHeals = heals.UpdateHeals
 
-	libheal.RegisterCallback(heals, "HealComm_HealStopped")
-	libheal.RegisterCallback(heals, "HealComm_HealDelayed")
-	libheal.RegisterCallback(heals, "HealComm_HealUpdated")
-	libheal.RegisterCallback(heals, "HealComm_HealStarted")
-	libheal.RegisterCallback(heals, "HealComm_ModifierChanged")
+    libheal.RegisterCallback(heals, "HealComm_HealStopped")
+    libheal.RegisterCallback(heals, "HealComm_HealDelayed")
+    libheal.RegisterCallback(heals, "HealComm_HealUpdated")
+    libheal.RegisterCallback(heals, "HealComm_HealStarted")
+    libheal.RegisterCallback(heals, "HealComm_ModifierChanged")
 end
 
 local frame
 function f:UNIT_AURA(event, unit)
-	frame = oUF.units[unit]
-	if not frame or frame.unit ~= unit then return end
+    frame = oUF.units[unit]
+    if not frame or frame.unit ~= unit then return end
 
-	local cur, tex, dis, dur, exp
-	local name, rank, buffTexture, count, duration, expire, dtype, isPlayer
-	for i = 1, 40 do
-		name, rank, buffTexture, count, dtype, duration, expire, isPlayer = UnitAura(unit, i, "HARMFUL")
-		if not name then break end
+    local cur, tex, dis, dur, exp
+    local name, rank, buffTexture, count, duration, expire, dtype, isPlayer
+    for i = 1, 40 do
+        name, rank, buffTexture, count, dtype, duration, expire, isPlayer = UnitAura(unit, i, "HARMFUL")
+        if not name then break end
 
-		if not cur or (debuffs[name] >= debuffs[cur]) then
-			if debuffs[name] > 0 and debuffs[name] > debuffs[cur or 1] then
-				-- Highest priority
-				cur = name
-				tex = buffTexture
-				dis = dtype or "none"
-				exp = expire
-				dur = duration
-			elseif dtype and dtype ~= "none" then
-				if not dis or (dispellPriority[dtype] > dispellPriority[dis]) then
-					cur = name
-					tex = buffTexture
-					dis = dtype
-					exp = expire
-					dur = duration
-				end
-			end
-		end
-	end
+        if not cur or (debuffs[name] >= debuffs[cur]) then
+            if debuffs[name] > 0 and debuffs[name] > debuffs[cur or 1] then
+                -- Highest priority
+                cur = name
+                tex = buffTexture
+                dis = dtype or "none"
+                exp = expire
+                dur = duration
+            elseif dtype and dtype ~= "none" then
+                if not dis or (dispellPriority[dtype] > dispellPriority[dis]) then
+                    cur = name
+                    tex = buffTexture
+                    dis = dtype
+                    exp = expire
+                    dur = duration
+                end
+            end
+        end
+    end
 
-	if dis then
-		local col = DebuffTypeColor[dis]
-		frame.border:SetVertexColor(col.r, col.g, col.b)
-		frame.Dispell = true
-		frame.border:Show()
-	elseif frame.Dispell then
-		if (curFrame ~= frame) or not curFrame then
-			frame.border:Hide()
-		elseif curFrame == frame then
-			frame.border:SetVertexColor(1, 1, 1)
-		end
-		frame.Dispell = False
-	elseif curFrame and curFrame == frame then
-		frame.border:SetVertexColor(1, 1, 1)
-	end
+    if dis then
+        local col = DebuffTypeColor[dis]
+        frame.border:SetVertexColor(col.r, col.g, col.b)
+        frame.Dispell = true
+        frame.border:Show()
+    elseif frame.Dispell then
+        if (curFrame ~= frame) or not curFrame then
+            frame.border:Hide()
+        elseif curFrame == frame then
+            frame.border:SetVertexColor(1, 1, 1)
+        end
+        frame.Dispell = False
+    elseif curFrame and curFrame == frame then
+        frame.border:SetVertexColor(1, 1, 1)
+    end
 
-	if (exp and exp > 0) and (dur and dur > 0) then
-		frame.cd:SetCooldown(exp - dur, dur)
-		frame.cd:Show()
-	else
-		frame.cd:Hide()
-	end
+    if (exp and exp > 0) and (dur and dur > 0) then
+        frame.cd:SetCooldown(exp - dur, dur)
+        frame.cd:Show()
+    else
+        frame.cd:Hide()
+    end
 
-	if cur then
-		frame.Icon:SetTexture(tex)
-		frame.Icon:Show()
-	else
-		frame.Icon:Hide()
-	end
+    if cur then
+        frame.Icon:SetTexture(tex)
+        frame.Icon:Show()
+    else
+        frame.Icon:Hide()
+    end
 end
 
 function f:PLAYER_TARGET_CHANGED()
-	local inRaid = UnitInRaid("target")
-	local frame
-	if inRaid then
-		if UnitExists("raid" .. inRaid + 1) then
-			frame = oUF.units["raid" .. inRaid + 1]
-		end
-	else
-		local name = UnitName("target")
-		if name == playerName and oUF.units.player then
-			frame = oUF.units.player
-		else
-			for i = 1, 4 do
-				if UnitExists("party" .. i) then
-					if name == UnitName("party" .. i) then
-						frame = oUF.units["party" .. i]
-						break
-					end
-				else
-					break
-				end
-			end
-		end
-	end
+    local inRaid = UnitInRaid("target")
+    local frame
+    if inRaid then
+        if UnitExists("raid" .. inRaid + 1) then
+            frame = oUF.units["raid" .. inRaid + 1]
+        end
+    else
+        local name = UnitName("target")
+        if name == playerName and oUF.units.player then
+            frame = oUF.units.player
+        else
+            for i = 1, 4 do
+                if UnitExists("party" .. i) then
+                    if name == UnitName("party" .. i) then
+                        frame = oUF.units["party" .. i]
+                        break
+                    end
+                else
+                    break
+                end
+            end
+        end
+    end
 
-	-- Deselected
-	if not frame then
-		if curFrame then
-			if not curFrame.Dispell then
-				curFrame.border:Hide()
-			end
-			curFrame = nil
-		end
-		return
-	end
+    -- Deselected
+    if not frame then
+        if curFrame then
+            if not curFrame.Dispell then
+                curFrame.border:Hide()
+            end
+            curFrame = nil
+        end
+        return
+    end
 
-	if curFrame then
-		if curFrame == frame then
-			if frame.Dispell then
-				return
-			else
-				frame.border:SetVertexColor(1, 1, 1)
-			end
-		else
-			curFrame.border:Hide()
-			if not frame.Dispell then
-				frame.border:SetVertexColor(1, 1, 1)
-				frame.border:Show()
-			end
-			curFrame = frame
-		end
-	else
-		if not frame.Dispell then
-			frame.border:SetVertexColor(1, 1, 1)
-			frame.border:Show()
-		end
-		curFrame = frame
-	end
+    if curFrame then
+        if curFrame == frame then
+            if frame.Dispell then
+                return
+            else
+                frame.border:SetVertexColor(1, 1, 1)
+            end
+        else
+            curFrame.border:Hide()
+            if not frame.Dispell then
+                frame.border:SetVertexColor(1, 1, 1)
+                frame.border:Show()
+            end
+            curFrame = frame
+        end
+    else
+        if not frame.Dispell then
+            frame.border:SetVertexColor(1, 1, 1)
+            frame.border:Show()
+        end
+        curFrame = frame
+    end
 end
 
 f:RegisterEvent("UNIT_AURA")
